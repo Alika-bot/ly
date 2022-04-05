1c21a3619e9b9b77fc0d73394ed46ea2747db962
melly <ekaputrimeisila@gmail.com>
2022-04-05 10:06:08 +0900

    ly

---
+++ settings.js
@@ -0,0 +1,59 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['‪6282288572356‬','6282288572356‬','6282288572356‬']
+global.premium = ['6282288572356‬']
+global.ownername = 'Mellyana'
+global.botname = 'AuBot'
+global.footer = 'Mellyana'
+global.ig = '-'
+global.email = 'mellyana42@gmail.com'
+global.region = 'English'
+global.sc = 'mo ngpain🗿'
+global.myweb = '-'
+global.packname = 'Mellyana'
+global.author = 'AuBot'
+global.sessionName = 'mellyana'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./image/chika.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
